361234bb3c6123fdd11bbc688651b4547a425b7f
Nathan Pietrantonio <pietrann@uci.edu>
2026-02-06 19:46:38 -0800

    Add docstrings

--- SyntheticDataGeneration/generate.py
+++ SyntheticDataGeneration/generate.py
@@ -3,6 +3,13 @@ import datetime
 import json
 
 def select_model():
+    """
+    Prompt the user to select a model from the available models on their machine.
+    Returns the selected model name.
+
+    Returns:
+        model: str - The selected model name.
+    """
     models = get_models()
     print()
     print("Available models:")
@@ -22,11 +29,29 @@ def select_model():
 
 
 def load_system_prompt(file_path):
+    """
+    Load the system prompt from the file path.
+    Returns the system prompt as a string.
+
+    Input:
+        file_path: str - The path to the system prompt file.
+    Returns:
+        system_prompt: str - The system prompt as a string.
+    """
     with open(file_path, 'r') as file:
         return file.read()
 
 
 def format_json(llm_json_string):
+    """
+    Format the JSON string returned by the LLM into a list of dictionaries.
+    Returns the list of dictionaries and adds a verified flag to each object.
+
+    Input:
+        llm_json_string: str - The JSON string returned by the LLM.
+    Returns:
+        json_obj: list - The list of dictionaries.
+    """
     cleaned_json_string = llm_json_string.replace('```json', '').replace('```', '')
     json_obj = json.loads(cleaned_json_string)
     # Add verified flag to each object
@@ -36,6 +61,10 @@ def format_json(llm_json_string):
 
 
 if __name__ == "__main__":
+    """
+    Main function to generate synthetic data.
+    Prompts the user to select a model, enter the number of examples to generate, and save the data.
+    """
     system = load_system_prompt('prompt.txt')
     model = select_model()
     num_examples = int(input("Enter the number of examples you want to generate: "))
@@ -44,7 +73,11 @@ if __name__ == "__main__":
     result = stream_response(model, prompt, system)[0]['content']
     print()
     formatted_json = format_json(result)
-    filename = f"generated_data_{datetime.datetime.now().strftime('%Y%m%d_%H%M%S')}.json"
-    with open(f'Output/{filename}', 'w') as file:
-        json.dump(formatted_json, file, indent=4)
-    print(f"Data saved to {filename}")
+    save = True if input("Save data? (y/n): ") == 'y' else False
+    if save:
+        filename = f"generated_data_{datetime.datetime.now().strftime('%Y%m%d_%H%M%S')}.json"
+        with open(f'Output/{filename}', 'w') as file:
+            json.dump(formatted_json, file, indent=4)
+        print(f"Data saved to {filename}")
+    else:
+        print("Data not saved")
--- SyntheticDataGeneration/ollama_api.py
+++ SyntheticDataGeneration/ollama_api.py
@@ -1,6 +1,16 @@
 import ollama
 
 def _setup_context(prompt, system=None, message_history=[]):
+    """
+    Set up message history for the LLM
+
+    Input:
+        prompt: str - The prompt to the LLM.
+        system: str - The system prompt as a string.
+        message_history: list - The message history as a list of dictionaries.
+    Returns:
+        messages: list - The messages as a list of dictionaries.
+    """
     messages = message_history + [{'role': 'user', 'content': prompt}]
     if system:
         messages.insert(0, {'role': 'system', 'content': system})
@@ -8,15 +18,43 @@ def _setup_context(prompt, system=None, message_history=[]):
 
 
 def _add_new_message(message_history, message):
+    """
+    Add a new message to the message history.
+
+    Input:
+        message_history: list - The message history as a list of dictionaries.
+        message: str - The message to add to the message history.
+    Returns:
+        message_history: list - The message history as a list of dictionaries.
+    """
     return message_history + [{'role': 'assistant', 'content': message}]
 
 
 def get_models():
+    """
+    Get the list of models available on the machine.
+
+    Returns:
+        models: list - The list of models as a list of strings.
+    """
     models = ollama.list().models
     return [model.model for model in models]
 
 
 def stream_response(model, prompt, system=None, message_history=[]):
+    """
+    Stream the response from the LLM.
+    This function outputs the response to the console in addition to
+    building a message history.
+
+    Input:
+        model: str - The model to use.
+        prompt: str - The prompt to the LLM.
+        system: str - The system prompt as a string.
+        message_history: list - The message history as a list of dictionaries.
+    Returns:
+        new_message_history: list - The message history as a list of dictionaries.
+    """
     messages = _setup_context(prompt, system, message_history)
     stream = ollama.chat(model=model, messages=messages, stream=True)
 
@@ -30,6 +68,17 @@ def stream_response(model, prompt, system=None, message_history=[]):
 
 
 def generate_response(model, prompt, system=None, message_history=[]):
+    """
+    Generate the response from the LLM.
+
+    Input:
+        model: str - The model to use.
+        prompt: str - The prompt to the LLM.
+        system: str - The system prompt as a string.
+        message_history: list - The message history as a list of dictionaries.
+    Returns:
+        new_message_history: list - The message history as a list of dictionaries.
+    """
     messages = _setup_context(prompt, system, message_history)
     response = ollama.chat(model=model, messages=messages)
     new_message_history = _add_new_message(message_history, response['message']['content'])
