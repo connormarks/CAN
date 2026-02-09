ff1760282084d9e08f7e68fa95d95ab38a3f4d8b
ConnorMarks <cmarks1@uci.edu>
2026-02-09 12:26:58 -0800

    classifier class in model.py

--- BERT/model.py
+++ BERT/model.py
@@ -1,1 +1,16 @@
 #BERT base model + emotion and topic heads
+
+import torch
+import torch.nn as nn
+from transformers import BertModel # huggingface
+
+
+#our bert model
+class EmotionTopicClassifier(nn.Module):
+    def __init__(self):
+        pass
+
+
+
+
+
