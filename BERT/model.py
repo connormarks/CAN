20f66376d9e61ec353e907fa756457a2c3969096
ConnorMarks <cmarks1@uci.edu>
2026-02-09 15:52:20 -0800

    still just playing around with the values, ADDED THE TOPIC AND EMOTION HEADS

--- BERT/model.py
+++ BERT/model.py
@@ -11,10 +11,22 @@ class EmotionTopicClassifier(nn.Module):
         super().__init__()
         self.bert = BertModel.from_pretrained("google-bert/bert-base-uncased") #THIS IS THE MODEL FROM TRANSFORMERS.
 
+        self.emotion_head = nn.Linear(768, 28)
+        self.topic_head = nn.Linear(768, 4)
+
+
 
 
 
 
 if __name__ == "__main__":
     our_bert = EmotionTopicClassifier()
+
+    print('\nEmotion Topic Classifier embeddings:\n')
     print(our_bert)
+
+    print('\nPre-trained model (baseline):\n')
+    print(our_bert.bert)
+    
+    print('\nconfig:\n')
+    print(our_bert.bert.config)
