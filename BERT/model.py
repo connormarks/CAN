b0ba089f1f136887dcfce7b4ebcd6ebf911fd98a
averyhorton <averyhortonlr@gmail.com>
2026-02-09 18:16:14 -0800

    Updated requirements.txt, added a dataset class for use in training, and added some torch compatibility to model.py

--- BERT/model.py
+++ BERT/model.py
@@ -1,17 +1,28 @@
-#BERT base model + emotion and topic heads
+# BERT base model + emotion and topic heads
 
 import torch
 import torch.nn as nn
 from transformers import BertModel # huggingface
 
-
-#our bert model
+# our bert model
 class EmotionTopicClassifier(nn.Module):
     def __init__(self):
         super().__init__()
-        self.bert = BertModel.from_pretrained("google-bert/bert-base-uncased") #THIS IS THE MODEL FROM TRANSFORMERS.
+        self.bert = BertModel.from_pretrained("google-bert/bert-base-uncased") # THIS IS THE MODEL FROM TRANSFORMERS.
+        hidden = self.bert.config.hidden_size # tracks size of hidden layer vectors for training
 
+        # Create the two heads for training; one for emotions, one for topic
+        self.emotion_head = nn.Linear(hidden, 28) 
+        self.topic_head = nn.Linear(hidden, 4)
 
+    def forward(self, input_ids, attention_mask): # used by torch in the training process
+        out = self.bert(
+            input_ids=input_ids,
+            attention_mask=attention_mask
+        )
+        cls = out.last_hidden_state[:, 0] # gets the first token for all samples in the batch
 
-
-
+        return {
+            "emotion_logits": self.emotion_head(cls),
+            "topic_logits": self.topic_head(cls)
+        }
