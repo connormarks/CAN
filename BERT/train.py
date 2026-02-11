715b3644e1eae304c54b6710bd36d1fc3cac6c73
ConnorMarks <cmarks1@uci.edu>
2026-02-10 21:17:00 -0800

    train.py commit for pull

--- BERT/train.py
+++ BERT/train.py
@@ -5,5 +5,10 @@ from model import EmotionTopicClassifier
 from dataset import preprocess_data
 import numpy as np
 import torch
-import random
 
+
+
+def train():
+    device = 'cuda' if torch.cuda.is_available() else 'cpu' 
+    emotion_topic_model = EmotionTopicClassifier().to(device) 
+    emotion_topic_model.train() # set the module in training mode
