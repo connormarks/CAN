fc603ea3a91040a91405f085a13671d660c68e01
ConnorMarks <cmarks1@uci.edu>
2026-02-11 14:26:47 -0800

    implemented adapters, slightly altered the preprocess data function in dataset.py, but kept original in comment, also modified the forward func, and fundamentally altered the train to use adapters, be batch-task-specific in its training including dataloading, and finally it now uses joint-optimized loss functions.

--- BERT/model.py
+++ BERT/model.py
@@ -12,13 +12,23 @@ class EmotionTopicClassifier(nn.Module):
     def __init__(self):
         super().__init__()
         self.bert = BertModel.from_pretrained("google-bert/bert-base-uncased") # THIS IS THE MODEL FROM TRANSFORMERS.
+
+        self.bert.add_adapter('emotion') # task-specific adapters for training
+        self.bert.add_adapter('topic')
+
+        self.bert.train_adapter(['emotion', 'topic']) #this line freezes the base model (making training more efficient)
+
         hidden = self.bert.config.hidden_size # tracks size of hidden layer vectors for training
 
         # Create the two heads for training; one for emotions, one for topic
         self.emotion_head = nn.Linear(hidden, 28) 
         self.topic_head = nn.Linear(hidden, 4)
 
-    def forward(self, input_ids, attention_mask): # used by torch in the training process
+
+    def forward(self, input_ids, attention_mask, task): # used by torch in the training process
+
+        self.bert.set_active_adapters(task)
+
         out = self.bert(
             input_ids=input_ids,
             attention_mask=attention_mask
@@ -29,6 +39,14 @@ class EmotionTopicClassifier(nn.Module):
             "emotion_logits": self.emotion_head(cls),
             "topic_logits": self.topic_head(cls)
         }
+
+
+
+
+
+
+
+
 
 # NOTE: Don't use this in final, I generated it with ChatGPT. I just used this to validate the preprocessing methods.
 # leaving it here for now for testing purposes
