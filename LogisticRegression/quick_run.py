57578e156d856c2b7bc20a1bfbbf068a4324e0f1
Nathan Pietrantonio <pietrann@uci.edu>
2026-02-10 18:37:47 -0800

    Fix quick run

--- LogisticRegression/quick_run.py
+++ LogisticRegression/quick_run.py
@@ -1,19 +1,23 @@
 from sklearn.metrics import accuracy_score
 from tools.dataset import load_datasets, create_test_train_split
-from tools.preprocess import preprocess_go_data, preprocess_ag_data
+from tools.preprocess import preprocess_go_data, preprocess_ag_data, create_vectorizer
 from tools.train import train_model
 
 if __name__ == "__main__":
+    # Quickly runs the models with very little preprocessing and analysis
+    # For more detailed analysis, see advanced_run.py
+
+    print("FOR MORE DETAILED ANALYSIS, SEE advanced_run.py\n")
+
     go_data, ag_data = load_datasets()
 
-    print("Preprocessing goemotion data...")
-    go_X, go_y = preprocess_go_data(go_data)
-    print("Preprocessing agnews data...\n")
-    ag_X, ag_y = preprocess_ag_data(ag_data)
+    texts = [*go_data["text"], *ag_data["Description"]]
+    vectorizer = create_vectorizer(texts)
 
-    print("Creating test train split...\n")
-    go_X_train, go_X_test, go_y_train, go_y_test = create_test_train_split(go_X, go_y)
-    ag_X_train, ag_X_test, ag_y_train, ag_y_test = create_test_train_split(ag_X, ag_y)
+    print("Preprocessing goemotion data...")
+    go_X_train, go_X_test, go_y_train, go_y_test = preprocess_go_data(go_data, vectorizer)
+    print("Preprocessing agnews data...")
+    ag_X_train, ag_X_test, ag_y_train, ag_y_test = preprocess_ag_data(ag_data, vectorizer)
 
     print("Fitting goemotion model...")
     go_model = train_model(go_X_train, go_y_train, max_iter=200)
