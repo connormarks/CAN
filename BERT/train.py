e0df191eb7167f5372c65c1ca29d896ec2f18b5d
ConnorMarks <cmarks1@uci.edu>
2026-02-10 22:45:22 -0800

    loss functions in traning loop.

--- BERT/train.py
+++ BERT/train.py
@@ -12,7 +12,8 @@ import torch
 essential:
     data loading
     forward pass
-    task-specific loss
+    task-specific loss (then combine)
+    backpropagate
     optimizer
 
 advanced techniques:
@@ -27,3 +28,23 @@ def train(dataloader):
     emotion_topic_model = EmotionTopicClassifier().to(device) #attach the device, this is in module.nn's to()
     emotion_topic_model.train() # set the module in training mode, also module.nn
 
+    for epoch in config.NUM_EPOCHS:
+        
+        for batch in dataloader:
+            input_ids = batch['input_ids'].to(device)
+            attention_mask = batch['attention_mask'].to(device)
+            emotion_labels = batch['emotion_labels'].to(device)
+            topic_labels = batch['topic_label'].to(device)
+
+
+            logits = emotion_topic_model.forward(input_ids, attention_mask) #logits is a dictionary
+            emotion_logits = logits['emotion_logits']
+            topic_logits = logits['topic_logits']
+
+            emotion_loss = torch.nn.BCEWithLogitsLoss(emotion_logits,emotion_labels)
+            topic_loss = torch.nn.CrossEntropyLoss(topic_logits, topic_labels, ignore_index=config.IGNORE_INDEX)
+
+            total_loss = emotion_loss + topic_loss
+
+
+  
