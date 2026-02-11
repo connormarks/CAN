1e325c6c654e6ab68dc4f3290504288b7f8d71f2
ConnorMarks <cmarks1@uci.edu>
2026-02-10 22:07:59 -0800

    takes a dataloader, loops through epochs and dataloader.

--- BERT/train.py
+++ BERT/train.py
@@ -6,9 +6,24 @@ from dataset import preprocess_data
 import numpy as np
 import torch
 
+'''
+-----Training loop notes----
 
+essential:
+    data loading
+    forward pass
+    task-specific loss
+    optimizer
 
-def train():
-    device = 'cuda' if torch.cuda.is_available() else 'cpu' 
-    emotion_topic_model = EmotionTopicClassifier().to(device) 
-    emotion_topic_model.train() # set the module in training mode
+advanced techniques:
+    task-specific adapters
+    gradient projection
+    dynamic loss-reweighting
+    task-specific schedulers
+'''
+
+def train(dataloader):
+    device = 'cuda' if torch.cuda.is_available() else 'cpu' #cuda = GPU, else cpu
+    emotion_topic_model = EmotionTopicClassifier().to(device) #attach the device, this is in module.nn's to()
+    emotion_topic_model.train() # set the module in training mode, also module.nn
+
