21022431559b98cf2992c94543fc9f515115560c
Nathan Pietrantonio <pietrann@uci.edu>
2026-02-11 20:00:33 -0800

    Implement gemini supoprt

--- SyntheticDataGeneration/generate.py
+++ SyntheticDataGeneration/generate.py
@@ -1,9 +1,10 @@
-from ollama_api import get_models, stream_response
+from ollama_api import get_models as get_ollama_models, stream_response as stream_ollama_response
+from gemini_api import get_models as get_gemini_models, stream_response as stream_gemini_response
 import datetime
 import json
 from config import OUTPUT_DIR
 
-def select_model():
+def select_model(api_models=False):
     """
     Prompt the user to select a model from the available models on their machine.
     Returns the selected model name.
@@ -11,22 +12,32 @@ def select_model():
     Returns:
         model: str - The selected model name.
     """
-    models = get_models()
+    if api_models:
+        models = get_gemini_models()
+        alt_choice = "Use Ollama"
+    else:
+        models = get_ollama_models()
+        alt_choice = "Use Gemini API"
+
     print()
     print("Available models:")
     for index, model in enumerate(models):
         print(f'\t {index}. {model}')
+    print(f'\t {len(models)}. {alt_choice}')
     print()
     choice = input("Enter the model you want to use: ")
     try:
         choice = int(choice)
+        if choice == len(models):
+            return select_model(api_models=not api_models)
         model = models[int(choice)]
     except Exception:
         print(f"Invalid choice")
-        return select_model()
+        return select_model(api_models)
+
     print(f"Selected model: {model}")
     print()
-    return model
+    return api_models, model
 
 
 def load_system_prompt(file_path):
@@ -68,16 +79,21 @@ if __name__ == "__main__":
     Prompts the user to select a model, enter the number of examples to generate, and save the data.
     """
     system = load_system_prompt('prompt.txt')
-    model = select_model()
+    is_api, model = select_model()
     num_examples = int(input("Enter the number of examples you want to generate: "))
     print()
     prompt = f"Please generate {num_examples} examples"
-    result = stream_response(model, prompt, system)[0]['content']
+
+    if is_api:
+        result = stream_gemini_response(model, prompt, system)['contents'][0]['parts'][0]['text']
+    else:
+        result = stream_ollama_response(model, prompt, system)[0]['content']
+
     print()
     formatted_json = format_json(result)
     save = True if input("Save data? (y/n): ") == 'y' else False
     if save:
-        filename = f"generated_data_{datetime.datetime.now().strftime('%Y%m%d_%H%M%S')}.json"
+        filename = f"{model.strip('models/')}_{datetime.datetime.now().strftime('%Y%m%d_%H%M%S')}.json"
         with open(f'{OUTPUT_DIR}/{filename}', 'w') as file:
             json.dump(formatted_json, file, indent=4)
         print(f"Data saved to {filename}")
--- SyntheticDataGeneration/ollama_api.py
+++ SyntheticDataGeneration/ollama_api.py
@@ -1,33 +1,5 @@
 import ollama
-
-def _setup_context(prompt, system=None, message_history=[]):
-    """
-    Set up message history for the LLM
-
-    Input:
-        prompt: str - The prompt to the LLM.
-        system: str - The system prompt as a string.
-        message_history: list - The message history as a list of dictionaries.
-    Returns:
-        messages: list - The messages as a list of dictionaries.
-    """
-    messages = message_history + [{'role': 'user', 'content': prompt}]
-    if system:
-        messages.insert(0, {'role': 'system', 'content': system})
-    return messages
-
-
-def _add_new_message(message_history, message):
-    """
-    Add a new message to the message history.
-
-    Input:
-        message_history: list - The message history as a list of dictionaries.
-        message: str - The message to add to the message history.
-    Returns:
-        message_history: list - The message history as a list of dictionaries.
-    """
-    return message_history + [{'role': 'assistant', 'content': message}]
+from context import setup_context, add_new_message
 
 
 def get_models():
@@ -55,7 +27,7 @@ def stream_response(model, prompt, system=None, message_history=[]):
     Returns:
         new_message_history: list - The message history as a list of dictionaries.
     """
-    messages = _setup_context(prompt, system, message_history)
+    _, messages = setup_context(prompt, system, message_history)
     stream = ollama.chat(model=model, messages=messages, stream=True)
 
     message = ''
@@ -63,7 +35,7 @@ def stream_response(model, prompt, system=None, message_history=[]):
         message += chunk['message']['content']
         print(chunk['message']['content'], end='', flush=True)
 
-    new_message_history = _add_new_message(message_history, message)
+    new_message_history = add_new_message(message_history, message)
     return new_message_history
 
 
@@ -79,7 +51,7 @@ def generate_response(model, prompt, system=None, message_history=[]):
     Returns:
         new_message_history: list - The message history as a list of dictionaries.
     """
-    messages = _setup_context(prompt, system, message_history)
+    _, messages = setup_context(prompt, system, message_history)
     response = ollama.chat(model=model, messages=messages)
-    new_message_history = _add_new_message(message_history, response['message']['content'])
+    new_message_history = add_new_message(message_history, response['message']['content'])
     return new_message_history
