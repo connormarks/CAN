95de4476687c1ecf637cefe31f5d13f9a4ca20b0
ConnorMarks <cmarks1@uci.edu>
2026-02-13 16:30:18 -0800

    wrote validate, mostly the same code, just checks the best loss and updates the patients variables.

--- BERT/train.py
+++ BERT/train.py
@@ -17,7 +17,7 @@ def train(dataloader):
     emotion_loss_bce = torch.nn.BCEWithLogitsLoss() #instantiate first outside the loop
     topic_loss_ce = torch.nn.CrossEntropyLoss(ignore_index=config.IGNORE_INDEX)
 
-    max_loss = float('-inf') #max validation loss for the patience metric
+    max_loss = float('inf') #max validation loss for the patience metric
     patience = 2 #epochs allowed without any improvement ^^
     patience_counter = 0
 
@@ -52,11 +52,47 @@ def train(dataloader):
 
             sum_loss += total_loss.item() #extracts the scalar number and adds to sum outside loop
         
+        
         # patience validation loss
         validation_loss = validate(emotion_topic_model, dataloader, device)
+
+        if validation_loss < max_loss:
+            max_loss = validation_loss
+            patience_counter = 0
+            torch.save(emotion_topic_model.state_dict(), "best.pt") # you can save the models dict which has the values of every tensor / parameter values
+        else:
+            patience_counter+=1
+
+        if patience_counter >= patience: # this is our limit
+            break
 
 
 
 
 def validate(emotion_topic_model, dataloader, device):
-    pass
+    emotion_topic_model.eval() # evaluation mode now, not training
+
+    sum_loss = 0.0
+
+    emotion_loss_bce = torch.nn.BCEWithLogitsLoss()
+    topic_loss_ce = torch.nn.CrossEntropyLoss(ignore_index=config.IGNORE_INDEX)
+
+    with torch.no_grad(): #context manager prevents permanent storage (goes away each loop)
+        for batch in dataloader:
+            #following code is copy pasted from above
+            input_ids = batch['input_ids'].to(device)
+            attention_mask = batch['attention_mask'].to(device)
+            emotion_labels = batch['emotion_labels'].to(device)
+            topic_labels = batch['topic_label'].to(device)
+            logits = emotion_topic_model(input_ids, attention_mask)
+            emotion_logits = logits['emotion_logits']
+            topic_logits = logits['topic_logits']
+            emotion_mask = (emotion_labels != -100).any(dim=1)
+            emotion_loss = emotion_loss_bce(emotion_logits[emotion_mask], emotion_labels[emotion_mask]) if emotion_mask.any() else torch.tensor(0.0, device=device) #task-specific loss
+            topic_loss = topic_loss_ce(topic_logits, topic_labels)
+
+            sum_loss += (emotion_loss+topic_loss).item()
+    
+    emotion_topic_model.train() #back to training mode
+
+    return sum_loss / len(dataloader) #average validation loss per batch
