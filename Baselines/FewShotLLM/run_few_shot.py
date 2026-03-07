ef7c5c43a0d5299abe425e0a3b921dc91a0c8fb5
Nathan Pietrantonio <pietrann@uci.edu>
2026-03-07 15:23:10 -0800

    Add data loading

--- Baselines/FewShotLLM/run_few_shot.py
+++ Baselines/FewShotLLM/run_few_shot.py
@@ -1,5 +1,26 @@
 from custom_llm_tools.tools import select_model
+import json
+import os
+
+DATA_PATH = '../../SyntheticDataGeneration/Output/Merged/merged_data.json'
+
+def _load_data(data_path=DATA_PATH):
+    """
+    Load the data from the default data path, or prompt the user for a different path.
+    Returns the data as a list of dictionaries.
+    """
+    # Check if the file exists
+    print(f"Loading data from {data_path}...")
+    if not os.path.exists(data_path):
+        print(f"File does not exist at {data_path}")
+        data_path = input("Enter the path to the data file: ")
+        return _load_data(data_path)
+    # Load the data
+    with open(data_path, 'r') as file:
+        data = json.load(file)
+        print(f"Loaded {len(data)} examples\n")
+        return data
 
 if __name__ == "__main__":
     is_api, model = select_model()
-    print(f"Selected model: {model}")
+    data = _load_data()
